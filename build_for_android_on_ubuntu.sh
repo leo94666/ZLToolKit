a66ecbd92560397b93184a615593a1b16d9dccb2
li <719947897@ qq.com>
2024-04-19 15:54:59 +0800

    add build_for_android_on_ubuntu

---
+++ build_for_android_on_ubuntu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt install cmake
+
+abi=$1
+
+cd ..
+git clone --depth=50 https://github.com/xia-chu/ZLToolKit.git
+cd ZLToolKit
+mkdir -p android_build
+rm -rf ./build
+ln -s ./android_build ./build
+cd android_build 
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK_ROOT  -DCMAKE_BUILD_TYPE=Release  -DANDROID_ABI=${abi} -DANDROID_NATIVE_API_LEVEL=android-16 -DCMAKE_INSTALL_PREFIX=../out/${abi}/
+make -j4
+sudo make install
