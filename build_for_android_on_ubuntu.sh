79b196a13491f82a786848c960f03530e0b42687
li <719947897@ qq.com>
2024-04-19 16:17:26 +0800

    modify CMakeLists.txt to fix android

--- build_for_android_on_ubuntu.sh
+++ build_for_android_on_ubuntu.sh
@@ -7,10 +7,11 @@ abi=$1
 cd ..
 git clone --depth=50 https://github.com/leo94666/ZLToolKit.git
 cd ZLToolKit
+sudo rm -rf android_build
 mkdir -p android_build
 rm -rf ./build
 ln -s ./android_build ./build
 cd android_build 
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK_ROOT  -DCMAKE_BUILD_TYPE=Release  -DANDROID_ABI=${abi} -DANDROID_NATIVE_API_LEVEL=android-16 -DCMAKE_INSTALL_PREFIX=../out/${abi}/
+cmake .. -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK_ROOT  -DCMAKE_BUILD_TYPE=Release  -DANDROID_ABI=${abi} -DANDROID_NATIVE_API_LEVEL=android-16 -DCMAKE_INSTALL_PREFIX=../out/${abi}/
 make -j4
 sudo make install
